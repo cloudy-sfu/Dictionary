c33254a4668544bb9f749cf6ffc5539345750582
cloudy-sfu <cloudy_stats@outlook.com>
2021-09-15 18:14:37 +0800

    v0.1.3

--- main.py
+++ main.py
@@ -1,95 +1,51 @@
-import json
-from requests import Session
 import os
+import json
+import config
 from playsound import playsound
+from requests import Session
 
-
-def url(lang: str, word: str) -> str:
-    return f"https://api.dictionaryapi.dev/api/v2/entries/{lang}/{word}"
-
-
-try:
-    with open('welcome', 'r') as f:
-        welcome = f.read()
-    with open('config.json', 'r') as f:
-        config = json.load(f)
-    with open('language_list', 'r') as f_:
-        language_list = f_.read()
-except (FileNotFoundError, json.JSONDecodeError):
-    input('Program files incomplete. Press enter to exit.')
-    exit(0)
 os.system('')
-print(welcome)
+print(config.welcome)
 sess = Session()
 sess.trust_env = False
 
-
-def set_language(*args):
-    config['language'] = args[0]
-
-
-def display_language_table(*args):
-    print(language_list)
-
-
-def display_history(*args):
-    print(', '.join(config['history']))
-
-
-def clear_history(*args):
-    config['history'] = []
-
-
-def save():
-    with open('config.json', 'w') as f_:
-        json.dump(config, f_)
-
-
-def set_tracking_preference(*args):
-    config['track'] = args[0]
-
-
-def play_pronunciation(*args):
-    playsound('https:' + args[0])
-
-
-hashed_function = {
-    "ls": set_language, "ll": display_language_table,
-    "hp": display_history, "hc": clear_history, "ht": set_tracking_preference,
-    "p": play_pronunciation,
-    "s": save,
-}
-
-
 while True:
-    query = input("> ")
-    if query[0] == '@':
-        cmd = query[1:].split()
-        if cmd[0] in hashed_function.keys():
-            hashed_function[cmd[0]](*cmd[1:])
-        else:
-            print("This command is not supported.")
-    else:
-        response = sess.get(url(config['language'], query), verify=True)
-        if response.status_code != 200:
-            print('Network error: status =', response.status_code)
-            print('* The 404 error will occur if the word does not exist.')
-        else:
-            if config['track'] == 'on':
-                config['history'] = [query] + config['history']
-            meaning = json.loads(response.text)[0]
-            display = ['\33[35m' + meaning['word'] + '\33[0m', '\33[31mPhonetics:\33[0m']
-            for phonetic in meaning['phonetics']:
-                display.append('    ' + phonetic['text'] + '    ' +
-                               phonetic['audio'] if 'audio' in phonetic.keys() else '')
-            display.append('\33[31mMeaning:\33[0m')
-            for meaning_ in meaning['meanings']:
-                if 'partOfSpeech' in meaning_.keys():
-                    display.append('    \33[33m' + meaning_['partOfSpeech'] + '\33[0m')
+    query = input(f'({config.language_code}) > ')
+    if query == '-lang':
+        print('| code\t | language\t |\n'
+              '-----------------------------')
+        [print(f'| {code}\t | {language}\t |') for code, language in config.language_list.items()]
+        config.language_code = input('New language code: ')
+        continue
+    response = sess.get(f"https://api.dictionaryapi.dev/api/v2/entries/{config.language_code}/{query}", verify=True)
+    if response.status_code != 200:
+        print('Network error: status =', response.status_code)
+        print('* The 404 error will occur if the word does not exist.')
+        continue
+    meaning = json.loads(response.text)[0]
+    if 'word' not in meaning.keys():
+        print("Cannot parse response data from Free Dictionary API.")
+        continue
+    print('\33[35m' + meaning['word'] + '\33[0m')
+    print('\33[31mPhonetics:\33[0m')
+    if 'phonetics' in meaning.keys():
+        for phonetic in meaning['phonetics']:
+            if 'text' in phonetic.keys():
+                print('\t' + phonetic['text'])
+            if 'audio' in phonetic.keys() and config.play_sound:
+                try:
+                    playsound('https:' + phonetic['audio'])
+                except:
+                    config.play_sound = False
+    print('\33[31mMeaning:\33[0m')
+    if 'meanings' in meaning.keys():
+        for meaning_ in meaning['meanings']:
+            if 'partOfSpeech' in meaning_.keys():
+                print('\t\33[33m' + meaning_['partOfSpeech'] + '\33[0m')
+            if 'definitions' in meaning_.keys():
                 for def_ in meaning_['definitions']:
-                    display.append(def_['definition'])
+                    print(def_['definition'])
                     if 'example' in def_.keys() and def_['example']:
-                        display.append('    \33[32me.g. ' + def_['example'] + '\33[0m')
+                        print('\t\33[32me.g. ' + def_['example'] + '\33[0m')
                     if 'synonyms' in def_.keys() and def_['synonyms']:
-                        display.append('    \33[34msynonyms: ' + ', '.join(def_['synonyms']) + '\33[0m')
-            print(*display, sep='\n')
+                        print('\t\33[34msynonyms: ' + ', '.join(def_['synonyms']) + '\33[0m')
